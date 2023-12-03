88103c56231ec7f6eb655d1218820e3dadf097b8
fmfahath <fmfahath.dev@gmail.com>
2023-12-04 00:32:03 +0530

    JS : questions array created

---
+++ js/app.js
@@ -0,0 +1,41 @@
+const questions = [
+
+    {
+        question: "Which is the largest animal?",
+        answers: [
+            {text: "Sark", correct: false},
+            {text: "Blue whale", correct: true},
+            {text: "Elephant", correct: false},
+            {text: "Giraffe", correct: false},
+        ]
+    },
+    {
+        question: "Which is not a country in the world map?",
+        answers: [
+            {text: "Isreal", correct: true},
+            {text: "Maldives", correct: false},
+            {text: "Syria", correct: false},
+            {text: "Palestine", correct: false},
+        ]
+    },
+    {
+        question: "largest Desrt?",
+        answers: [
+            {text: "Kalhari", correct: false},
+            {text: "Gobi", correct: false},
+            {text: "Sahara", correct: true},
+            {text: "Saudi", correct: false},
+        ]
+    },
+    {
+        question: "Which one is a space research company?",
+        answers: [
+            {text: "Google", correct: false},
+            {text: "Tesla", correct: false},
+            {text: "Spce X", correct: true},
+            {text: "Microsoft", correct: false},
+        ]
+    },
+];
+
+console.log(questions);
